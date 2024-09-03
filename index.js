be75bbc21202a36b4fcb14f92cf20e9d729b08e4
tehkali <ma5108480@gmail.com>
2024-09-03 11:03:39 +0500

    Incriment/Decriment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var sum = 2 + 3;
-var diff = 99 - 33;
-var prod = 14 * 2.9;
-var divid = 78 / 6;
-console.log([sum, diff, prod, divid].join('\n'))
+
+// incriment 
+
+myVar = 10;
+myVar++;
+
+// Decriment
+
+myVar = 10;
+myVar--;
