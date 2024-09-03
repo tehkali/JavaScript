8ab153d1b05d90c2cdbe2880f5a0124224000987
tehkali <ma5108480@gmail.com>
2024-09-03 11:41:44 +0500

    Augumented Math  Operation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+//Augumented Operation 
+var a = 3;
+var b = 8;
+var c = 12
 
-//Decimal is like integers in JavaScript e.g Multiplying/Dividing etc..
-//Reminder & Even/Odd
-//if number divided by 2 its even else odd
-
-var reminder = 99 % 2;
-// answer is 1 its mean not even
+a = a + 2;
+b = b - 6;
+c = c / 2;
+//We also write the code is
+a += 2;
+b -= 6;
+c /= 2;
