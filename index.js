1e406f78bd7511603c3f425ecb4b6446912bd378
tehkali <ma5108480@gmail.com>
2024-09-03 12:06:00 +0500

    Slashes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-//Declaring STRINGs
-var firstName = "Muhammad";
-var secondName = 'Ahmad';
 
-//Qoutation within String
-var mystr = "I am \"Muhammad Ahmad\" Learing \"JavaScript\"";
-//OR using with single quotes doesn't need Slashes
-var mystr1 = 'I am "Ahmad" Learing "JavaScript"';
+/*
+-- SLASHEs --
+\': Inserts a single quote in a string.
+\": Inserts a double quote in a string.
+\\: Inserts a backslash in a string.
+\n: Inserts a newline in a string.
+\r: Moves the cursor to the beginning of the line.
+\t: Inserts a tab space in a string.
+\b: Deletes the previous character in a string.
 
-
-console.log(mystr1)
+*/
